cb28c9dadc5b75aab39bd722f80a1b6f7b6dd83e
Noemi <noemi10663@gmail.com>
2022-10-20 15:18:11 -0400

    Create function getComputerChoice

---
+++ script.js
@@ -0,0 +1,14 @@
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random()*3);
+    if (randomNumber === 0){
+    return("Rock");
+}
+    else if(randomNumber === 1){
+    return ("Paper");
+}
+    else if (randomNumber === 2){
+    return ("Scissors");
+}
+}
+    console.log(getComputerChoice());
+
